91d37f90a0d538642e4cc8d773d5200c742f1fe4
bntr <none@none>
2019-09-14 04:49:18 +0300

    Rationals.Explorer: run Avalonia app
    --HG--
    branch : Avalonia

---
+++ src/Rationals.Explorer/Explorer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Diagnostics;
+
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.Logging.Serilog;
+using Avalonia.Styling;
+
+namespace Rationals.Explorer
+{
+    public class App : Application {
+        public override void Initialize() {
+            // Look up csproj's AvaloniaResource-s,
+            // find an Application config for "Rationals.Explorer.App"
+            // and load it
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+
+    class Program
+    {
+        // Your application's entry point. 
+        // Here you can initialize your MVVM framework, DI container, etc.
+        private static void AppMain(Application app, string[] args) {
+            //app.Run(new MainWindow());
+            Debug.WriteLine("AppMain reached");
+        }
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp() {
+            var appBuilder = AppBuilder.Configure<App>();
+            appBuilder.UsePlatformDetect();
+            //appBuilder.UseDataGrid();
+            appBuilder.LogToDebug(Avalonia.Logging.LogEventLevel.Information);
+            return appBuilder;
+        }
+
+        public static void Main(string[] args) {
+            var appBuilder = BuildAvaloniaApp();
+            appBuilder.Start(AppMain, args);
+        }
+    }
+}
